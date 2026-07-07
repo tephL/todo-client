cac4b53b2719479d466ca008dbcf48a43a17d4b6
teph <tephsahurrr@gmail.com>
2026-07-07 15:54:45 +0800

    PATCH: re-configured register response handling

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { NavLink } from "react-router";
 import { useNavigate } from "react-router";
 import * as authServ from '@/services/auth-serv.js';
 
@@ -25,20 +26,21 @@ function Register(){
 
         await authServ.registerUser({ username: newUser.username, password: newUser.password })
         .then(async res => {
-            console.log(res);
-            if(res.ok){
-                const data = await res.json();
-                console.log(data);
-                setRegisterMessage(data?.message || 'Successfully created');
-                setTimeout(() => navigate('/login', 3000));
-            }
             if(!res.ok){
                 const data = await res.json();
-                console.log(data);
-                setRegisterMessage(data?.message || '');
+                throw new Error(data?.message || "Something went wrong");
+            } else{
+                return res;
             }
         })
+        .then(res => {
+            console.log(res);
+            setRegisterMessage('Account successfully created');
+            setTimeout(() => { navigate('/login') }, 3000);
+        })
         .catch(e => {
+            console.log(e);
+            setRegisterMessage(e.message);
         });
     }
 
@@ -48,6 +50,7 @@ function Register(){
             Username: <input type="text" onChange={e => handleUsername(e.currentTarget.value)}/>
             Password: <input type="text" onChange={e => handlePassword(e.currentTarget.value)}/>
             <button onClick={register}>Register</button>
+            <p>Already have an account? <NavLink to={'/login'}>Login</NavLink></p>
         </div>
     )
 }
